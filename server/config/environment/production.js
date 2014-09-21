256d75a2c3ecabba0f74271dd49f82d97188e079
Adam Schodde <volvox@cloudy.mobile.asu.edu>
2014-09-21 13:17:56 -0700

    Updated production settings file.

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,11 +13,28 @@ module.exports = {
             process.env.PORT ||
             8080,
 
-  // MongoDB connection options
-  mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/returnmehome'
+  couchdb: {
+    url: 'http://localhost:5984',
+    users: 'lostandfound-user',
+    items: 'lostandfound-item'
+  },
+  cookie: {
+    secret: '' // 64+ key.
+  },
+  mashape: {
+    apiKey: '', // Mashape API key
+    metropolis: {
+      endpoint: 'https://metropolis-api-phone.p.mashape.com/analysis'
+    },
+    montanaFlynn: {
+      endpoint: 'https://montana-flynn-address-parser.p.mashape.com/parsed-address'
+    }
+  },
+  imgur: {
+    apiKey: '' // Imgur API key
+  },
+  aws: {
+    access_key: '', // AWS Access Key
+    secret_access_key: '' // AWS Secret Access Key
   }
 };
