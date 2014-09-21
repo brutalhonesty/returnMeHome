e39552fc839a304a644f66c71fd82e8cb6514c75
Adam Schodde <volvox@cloudy.mobile.asu.edu>
2014-09-21 13:15:32 -0700

    Removed API keys.

--- server/config/environment/development.template.js
+++ server/config/environment/development.template.js
@@ -3,7 +3,28 @@
 // Development specific configuration
 // ==================================
 module.exports = {
+  couchdb: {
+    url: 'http://localhost:5984',
+    users: 'lostandfound-user',
+    items: 'lostandfound-item'
+  },
+  cookie: {
+    secret: '' // 64+ key.
+  },
   mashape: {
-    apiKey: '' // API key for Mashape
+    apiKey: '', // Mashape API key
+    metropolis: {
+      endpoint: 'https://metropolis-api-phone.p.mashape.com/analysis'
+    },
+    montanaFlynn: {
+      endpoint: 'https://montana-flynn-address-parser.p.mashape.com/parsed-address'
+    }
+  },
+  imgur: {
+    apiKey: '' // Imgur API key
+  },
+  aws: {
+    access_key: '', // AWS Access Key
+    secret_access_key: '' // AWS Secret Access Key
   }
 };
